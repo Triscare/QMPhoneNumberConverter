684a8df66f422c3606e449d2b9290533610bb1c3
Klemm <felix.klemm@computerkomplett.de>
2019-04-04 11:33:44 +0200

    index on master: f7ddbb3 added config

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,7 @@
 public class main {
 
     public static void main(String[] args) {
-
+        Converter converter = new Converter();
+        converter.convertPhnoneNumber("+497411752320");
     }
 }
