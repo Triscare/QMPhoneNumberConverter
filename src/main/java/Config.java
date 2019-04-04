b64a48d85026a16b4737f6732722e9cac0320fc5
Klemm <felix.klemm@computerkomplett.de>
2019-04-04 14:43:28 +0200

    index on master: 2e2b019 end of second sprint

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,5 +1,8 @@
 public class Config {
 
+    /**
+     * The default country code of the application
+     */
     public static String defaultCountryCode="DE";
 
     private Config(){}
--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -7,14 +7,9 @@ import java.util.Locale;
 public class Converter {
 
     private PhoneNumberUtil phoneNumberUtil;
-    private String[] locales;
 
     public Converter(){
-
         phoneNumberUtil=PhoneNumberUtil.getInstance();
-
-        locales = Locale.getISOCountries();
-
     }
 
     public PhoneNumber convertPhnoneNumber(String phoneNumber){
@@ -31,7 +26,6 @@ public class Converter {
             else {
                 tempNumber=phoneNumberUtil.parse(phoneNumber, Config.defaultCountryCode);
             }
-
             String intNumber=phoneNumberUtil.format(tempNumber, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL);
 
             System.out.println(intNumber);
@@ -39,15 +33,6 @@ public class Converter {
 
             System.out.println(phoneNumberUtil.getRegionCodeForCountryCode(Integer.valueOf(splited[0].toString().substring(1))));
 
-//            if(phoneNumber.contains("-")){
-//
-//                String directDialing= phoneNumber.split("-")[1];
-//                if(splited[2].contains(directDialing)) {
-//                    splited[2] = splited[2].replace(directDialing, "");
-//                    return new PhoneNumber(splited[0], splited[1], splited[2], directDialing);
-//                }
-//            }
-            System.out.println(splited.length);
             if(splited.length==3) {
                 return new PhoneNumber(splited[0], splited[1], splited[2]);
             }
