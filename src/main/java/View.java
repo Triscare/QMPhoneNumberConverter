a20f936bdec44e961eae0ce5105c359e289a21fc
Christian Rohrmann <christian.rohrmann@gmx.de>
2019-04-04 15:42:47 +0200

    added javadoc

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -2,6 +2,9 @@ import java.io.Console;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+/**
+ * This will be the class which will read and display the phone numbers
+ */
 public class View {
     private final static String CHANGE_COUNTRY_CODE = "CountryCode:";
     private Converter converter;
@@ -9,34 +12,34 @@ public class View {
         converter=new Converter();
     }
 
+    /**
+     * This will continously read the console and after every new line a formatted Number will be outputted
+     * With 'CountryCode:en' the standard country code can be changed. The following signs should be an ISO Country Code
+     */
     public void startReading(){
             Console cnsl = null;
             Scanner scan = null;
 
             try {
-                // creates a console object
                 cnsl = System.console();
 
-                // if console is not null
                 if (cnsl != null) {
 
                     System.out.println("If you want to change the country Code please enter %n 'CountryCode:' %n followed by an ISO Country code like 'de'");
 
-                    // prints
-
-                    // create new scanner object
                     scan = new Scanner(cnsl.reader());
                     String number = "";
-                    // read till the end of data
                     System.out.print("Enter Phone Number : ");
+                    //a new String is emitted after every line.seperator (Enter is pressed)
                     Pattern delimiters = Pattern.compile(System.getProperty("line.separator"));
                     scan.useDelimiter(delimiters);
-                    //TODO Space signs will split the String somehow.
-
+                    //Continuosly scans the Console for new input
                     while (scan.hasNext()) {
-                        // read next
+                        //Enters when a enter is pressed
                         String str = scan.next();
                         number = str;
+
+                        //Is used for changing the Country Code
                         if(number.startsWith(CHANGE_COUNTRY_CODE)){
                             String[] code = number.split(":");
                             if(code.length == 1){
@@ -46,15 +49,11 @@ public class View {
                             }
                             System.out.println("CC: " + code[1]);
                             System.out.print("Enter Phone Number : ");
-                            number = "";
-                            str = "";
                             continue;
                         }
-                        System.out.println("Full Number found: " + number);
-                        converter.convertPhnoneNumber(number);
+
+                        System.out.println("Formatted Number: " + converter.convertPhnoneNumber(number));
                         System.out.print("Enter Phone Number : ");
-                        str = "";
-                        number = "";
 
                     }
                 }
